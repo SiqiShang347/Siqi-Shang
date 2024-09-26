d8f3e801db47d3c18d178fa4f49faf7d427dedc3
SiqiShang347 <18562080821@163.com>
2024-09-26 20:20:45 +0800

    Add files via upload

--- assignment-st-starter2.py
+++ assignment-st-starter2.py
@@ -8,6 +8,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 # show the title
 st.title('Titanic App by Siqi Shang')
 # read csv and show the dataframe
